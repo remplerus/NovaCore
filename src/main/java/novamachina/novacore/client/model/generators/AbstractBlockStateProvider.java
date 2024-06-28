91a775aa6eb8ee84b95d1c9d8d75acee6d565d7d
Jacob Williams <jacob.t.williams@outlook.com>
2024-06-28 15:41:09 -0500

    Add orrientable blocks

--- src/main/java/novamachina/novacore/client/model/generators/AbstractBlockStateProvider.java
+++ src/main/java/novamachina/novacore/client/model/generators/AbstractBlockStateProvider.java
@@ -1,13 +1,18 @@
 package novamachina.novacore.client.model.generators;
 
+import java.util.function.BiConsumer;
+import net.minecraft.core.Direction;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.material.Fluid;
 import net.neoforged.neoforge.client.model.generators.BlockStateProvider;
 import net.neoforged.neoforge.client.model.generators.ConfiguredModel;
 import net.neoforged.neoforge.client.model.generators.ModelFile;
+import net.neoforged.neoforge.client.model.generators.VariantBlockStateBuilder;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 
 public abstract class AbstractBlockStateProvider extends BlockStateProvider {
@@ -37,6 +42,57 @@ public abstract class AbstractBlockStateProvider extends BlockStateProvider {
   protected void simpleItemBlock(Block block, ModelFile modelFile) {
     simpleBlock(block, modelFile);
     simpleBlockItem(block, modelFile);
+  }
+
+  protected VariantBlockStateBuilder horrizontalOrrientable(
+      Block block, BiConsumer<BlockState, ConfiguredModel.Builder<?>> model) {
+    VariantBlockStateBuilder builder = getVariantBuilder(block);
+    builder.forAllStates(
+        state -> {
+          ConfiguredModel.Builder<?> bld = ConfiguredModel.builder();
+          model.accept(state, bld);
+          applyRotationBld(bld, state.getValue(BlockStateProperties.HORIZONTAL_FACING));
+          return bld.build();
+        });
+
+    simpleBlockItem(
+        block,
+        new ConfiguredModel(
+                models().withExistingParent(getRegistryName(block), mcLoc("block/cube")))
+            .model);
+
+    return builder;
+  }
+
+  protected VariantBlockStateBuilder orrientable(
+      Block block, BiConsumer<BlockState, ConfiguredModel.Builder<?>> model) {
+    VariantBlockStateBuilder builder = getVariantBuilder(block);
+    builder.forAllStates(
+        state -> {
+          ConfiguredModel.Builder<?> bld = ConfiguredModel.builder();
+          model.accept(state, bld);
+          applyRotationBld(bld, state.getValue(BlockStateProperties.FACING));
+          return bld.build();
+        });
+
+    simpleBlockItem(
+        block,
+        new ConfiguredModel(
+                models().withExistingParent(getRegistryName(block), mcLoc("block/cube")))
+            .model);
+
+    return builder;
+  }
+
+  private void applyRotationBld(ConfiguredModel.Builder<?> builder, Direction direction) {
+    switch (direction) {
+      case DOWN -> builder.rotationX(90);
+      case UP -> builder.rotationX(-90);
+      case NORTH -> {}
+      case SOUTH -> builder.rotationY(180);
+      case WEST -> builder.rotationY(270);
+      case EAST -> builder.rotationY(90);
+    }
   }
 
   protected String getRegistryName(Block b) {
