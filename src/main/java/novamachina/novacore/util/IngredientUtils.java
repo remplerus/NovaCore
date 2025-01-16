c6359e26c717f561fbf1c5406be717f78f7d60ad
Jacob Williams <jacob.t.williams@outlook.com>
2025-01-15 18:19:01 -0600

    Update to 1.21.4

--- src/main/java/novamachina/novacore/util/IngredientUtils.java
+++ src/main/java/novamachina/novacore/util/IngredientUtils.java
@@ -1,8 +1,5 @@
 package novamachina.novacore.util;
 
-import net.minecraft.core.Holder;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
 import org.slf4j.Logger;
 
@@ -20,11 +17,11 @@ public class IngredientUtils {
    * @return true if test is a subset of source, false otherwise
    */
   public static boolean isIngredientIn(Ingredient test, Ingredient source) {
-    for (Holder<Item> stack : test.items()) {
-      if (source.test(new ItemStack(stack))) {
-        return true;
-      }
-    }
+    //    for (Holder<Item> stack : test.items()) {
+    //      if (source.test(new ItemStack(stack))) {
+    //        return true;
+    //      }
+    //    }
     return false;
   }
 }
